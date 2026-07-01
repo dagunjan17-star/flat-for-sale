8f717d9c13b50ec2b9ccb39e923ae54f18462d39
Mukesh Kumar <mk8701952@gmail.com>
2026-07-01 13:31:08 +0530

    auto update

--- src/components/GurgaonRealEstateSection.jsx
+++ src/components/GurgaonRealEstateSection.jsx
@@ -58,8 +58,26 @@ const faqs = [
 
 export default function GurgaonRealEstateSection() {
   const [openIndex, setOpenIndex] = useState(0);
-
+const faqSchema = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  mainEntity: faqs.map((faq) => ({
+    "@type": "Question",
+    name: faq.question,
+    acceptedAnswer: {
+      "@type": "Answer",
+      text: faq.answer,
+    },
+  })),
+};
   return (
+    <>
+     <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify(faqSchema),
+    }}
+  />
    <section className="relative overflow-hidden bg-white py-10 px-4 sm:px-6">
 
   {/* Background Lights */}
@@ -175,5 +193,6 @@ export default function GurgaonRealEstateSection() {
     </div>
   </div>
 </section>
+</>
   );
 }
